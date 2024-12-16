4950d2a5b92fe1845514ae964e418b3cee258e35
Abdullah Onur Keklik <ao_keklik@hotmail.com>
2024-12-16 22:21:22 +0100

    Add admin password reset functionality with email verification.

---
+++ admin/auth_forget.php
@@ -0,0 +1,73 @@
+<?php include "./layout/top.php"?>
+<?php
+    if(Form::has_submit("form")) {
+
+        if(Form::required("email"))
+            Form::push_error("email","The email field is required!");
+
+        if(Form::email("email"))
+            Form::push_error("email","Email must be valid!");
+
+        if(!Form::exist_email($pdo,"admins","email"))
+            Form::push_error("email","The provided email is not registered!");
+
+        if (!Form::has_error()) {
+            try {
+                $token = bin2hex(random_bytes(32 / 2));
+                $sql = "update admins set token=:token,status=:status where email=:email";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(":email",Form::get_data("email"));
+                $stmt->bindValue(":token",$token);
+                $stmt->bindValue(":status",0);
+                if($stmt->execute()) {
+                    $hashedToken = password_hash($token, PASSWORD_DEFAULT); 
+                    $message = "<p>Please click on the following link in order to redet the password.</p>";
+                    $message .= "<a href='".ADMIN_URL."auth_reset.php?email=".Form::get_data("email")."&token=".$hashedToken."'>Reset Password!</a>";
+                    if (
+                        Mail::to(Form::get_data("email"))
+                        ->subject("Reset Email")
+                        ->message($message)
+                        ->send()
+                    ) {
+                        Redirect::route("auth_login.php")->with("success","Please check your email and follow the instruction to reset the password.");
+                    }
+                }
+            } catch (Exception $err) {
+                $error_message = $err->getMessage();
+            }
+        }
+    }
+?>
+<section class="section">
+    <div class="container container-login">
+        <div class="row">
+            <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                <div class="card card-primary border-box">
+                    <div class="card-header card-header-auth">
+                        <h4 class="text-center">Reset Password</h4>
+                    </div>
+                    <div class="card-body card-body-auth">
+                        <form method="POST" action="">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="email" placeholder="Email Address" value="<?php echo Form::get_old("email")?>" autofocus>
+                                <?php if(Form::has_error("email")): echo Form::get_error("email"); endif?>
+                            </div>
+                            <div class="form-group">
+                                <button name="form" type="submit" class="btn btn-primary btn-lg w_100_p">
+                                    Send Password Reset Link
+                                </button>
+                            </div>
+                            <div class="form-group">
+                                <div>
+                                    <a href="<?php echo ADMIN_URL?>auth_login.php">Back to login page</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include "../admin/layout/footer.php"?>
---
+++ admin/auth_reset.php
@@ -0,0 +1,73 @@
+<?php include "./layout/top.php"?> 
+<?php
+    if(Form::has_submit("form")) {
+        if(Form::required("password"))
+            Form::push_error("password","The password field is required!");
+
+        if(Form::minmax("min:8|max:20","password"))
+            Form::push_error("password","The Password must be between 8 and 20 characters!");
+
+        if(Form::required("confirm_password"))
+            Form::push_error("confirm_password","The confirm password field is required!");
+
+        if(!Form::same("password", "confirm_password"))
+            Form::push_error("confirm_password","Passwords do not match!");   
+
+        if(!Form::has_error()) {
+            try {
+                $sql = "select id,email,token from admins where email=:email limit 1";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(":email", htmlspecialchars(trim($_REQUEST["email"])));
+    
+                if($stmt->execute()) {
+                    $admin = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+                    if(!password_verify($admin["token"], htmlspecialchars(trim($_REQUEST["token"]))))
+                        return Redirect::route()->with("error","Invalid or missing reset credentials.");
+
+                    $sql = "update admins set password=:password,status=:status,token=:token where id=:id";
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->bindValue(":id", $admin["id"]);
+                    $stmt->bindValue(":password", password_hash(Form::get_data("password"), PASSWORD_DEFAULT));
+                    $stmt->bindValue(":status", 1);
+                    $stmt->bindValue(":token", NULL);
+    
+                    if($stmt->execute()) {
+                        Redirect::route("auth_login.php")->with("success","Your password has been successfully reset!");
+                    }
+                }
+            } catch(PDOException $err) {
+                $error_message = $err->getmessage();
+            }
+        }
+    }
+?>
+<section class="section">
+    <div class="container container-login">
+        <div class="row">
+            <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                <div class="card card-primary border-box">
+                    <div class="card-header card-header-auth">
+                        <h4 class="text-center">Reset Password</h4>
+                    </div>
+                    <div class="card-body card-body-auth">
+                        <form method="POST" action="">
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password" value="" autofocus>
+                                <?php if(Form::has_error("password")): echo Form::get_error("password"); endif?>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="confirm_password" placeholder="Retype Password" value="">
+                                <?php if(Form::has_error("confirm_password")): echo Form::get_error("confirm_password"); endif?>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" name="form" class="btn btn-primary btn-lg w_100_p">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include "../admin/layout/footer.php"?>
