692fdea5752d56a8cf9ecdbcd765720685c9f07d
Abdullah Onur Keklik <ao_keklik@hotmail.com>
2024-12-18 18:33:56 +0100

    Create login page with user type selection (customer or realtor)

---
+++ auth_select_user.php
@@ -0,0 +1,43 @@
+<?php include "./layout_header.php"?>
+
+<div class="page-top" style="background-image: url('https://placehold.co/1300x260')">
+    <div class="bg"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Login</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="page-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+            
+                <div class="main-part-user">
+                    <div class="left-part-user">
+                        <h3>
+                            <a href="<?php echo BASE_URL?>customer-login">Customer Registration</a>
+                        </h3>
+                        <h3>
+                            <a href="<?php echo BASE_URL?>customer-register">Customer Login</a>
+                        </h3>
+                    </div>
+                    <div class="right-part-user">
+                        <h3>
+                            <a href="<?php echo BASE_URL?>agent-login">Agent Registration</a>
+                        </h3>
+                        <h3>
+                            <a href="<?php echo BASE_URL?>agent-register">Agent Login</a>
+                        </h3>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "./layout_footer.php"?>
